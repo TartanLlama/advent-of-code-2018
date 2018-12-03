139836077580d4864d63c2d9165d348a82bf1d99
Simon Brand <tartanllama@gmail.com>
2018-12-03 13:48:54 +0000

    Better solution for part 2

--- advent_of_code_2018/2_inventory_management_system/part2.cpp
+++ advent_of_code_2018/2_inventory_management_system/part2.cpp
@@ -1,31 +1,26 @@
 #include <range/v3/istream_range.hpp>
-#include <range/v3/numeric/accumulate.hpp>
+#include <range/v3/view/filter.hpp>
+#include <range/v3/view/cartesian_product.hpp>
 #include <range/v3/view/zip.hpp>
-#include <range/v3/distance.hpp>
-#include <range/v3/view/remove_if.hpp>
+#include <range/v3/numeric/accumulate.hpp>
+#include <range/v3/view/take.hpp>
 #include <functional>
 #include <iostream>
-#include "tl-range/k_combinations.hpp"
 
 using namespace ranges;
 
-template <class Rng1, class Rng2>
-int hamming_distance(Rng1&& r1, Rng2&&r2) {
-    return accumulate(view::zip(r1,r2), 0, ranges::plus{},
-                     [](auto&& x) { return x.first != x.second; });
-}
-
-auto second = [](auto&& p) { return p.second; };
-
 int main() {
-    auto ns = ranges::istream_range<std::string>(std::cin) | to_vector;
-    for (auto i = begin(ns); i != end(ns); ++i) {
-        for (auto j = next(i); j != end(ns); ++j) {
-            if (hamming_distance(*i, *j) == 1) {
-              for (auto idx = 0; idx < i->size(); ++idx){
-                  if((*i)[idx] == (*j)[idx]) std::cout << (*i)[idx];
-              }
-            }
-        }
-    }
+	auto hamming_distance = [](auto&& r1, auto&& r2) { 
+		return accumulate(view::zip(r1, r2), 0, ranges::plus{},
+			[](auto&& x) { return x.first != x.second; }); 
+	};
+
+	auto ns = ranges::istream_range<std::string>(std::cin) | to_vector;
+	auto found = view::cartesian_product(ns, ns) 
+		       | view::filter([&](auto&& p) { return hamming_distance(get<0>(p), get<1>(p)) == 1; });
+	for (auto [s1,s2] : found | view::take(1)) {
+		for (auto[c1, c2] : view::zip(s1, s2)) {
+			if (c1 == c2) std::cout << c1;
+		}
+	}
 }
