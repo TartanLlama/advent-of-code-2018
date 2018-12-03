94f3c0389a9a18ddc1b7bc0e1984f6d864ff358d
Simon Brand <tartanllama@gmail.com>
2018-12-03 11:19:44 +0000

    Day 3 part 2

--- advent_of_code_2018/3_no_matter_how_you_slice_it/part2.cpp
+++ advent_of_code_2018/3_no_matter_how_you_slice_it/part2.cpp
@@ -6,9 +6,12 @@
 #include <range/v3/view/indices.hpp>
 #include <range/v3/numeric/accumulate.hpp>
 #include <range/v3/view/join.hpp>
+#include <range/v3/algorithm/find_if.hpp>
+#include <range/v3/view/filter.hpp>
 #include <string_view>
 #include <iostream>
 #include <map>
+#include <vector>
 #include "tuple_drop.hpp"
 
 using namespace ranges;
@@ -27,18 +30,20 @@ rectangle parse_rectangle(std::string_view s) {
 }
 
 int main() {
-	enum class state : char { unclaimed, claimed, hot };
-	std::vector<state> hot_map(1'000'000);
-	auto rects = getlines(std::cin) | view::transform(parse_rectangle);
+	auto rects = getlines(std::cin) | view::transform(parse_rectangle) | to_vector;
+
+	std::vector<std::vector<short>> id_hits(1'000'000);
+	std::vector<bool> ids_intersected(rects.size());
 	for (auto&& r : rects) {
-		for (auto i : view::closed_indices(r.left, r.width+r.left-1)) {
-			for (auto j : view::closed_indices(r.top, r.height+r.top-1)) {
-				auto& e = hot_map[i*1000 + j];
-				if (e == state::claimed) e = state::hot;
-				else if (e == state::unclaimed) e = state::claimed;
+		for (auto i : view::closed_indices(r.left, r.width + r.left - 1)) {
+			for (auto j : view::closed_indices(r.top, r.height + r.top - 1)) {
+				id_hits[i * 1000 + j].push_back(r.id);
 			}
 		}
 	}
-
-	std::cout << accumulate(hot_map, 0, plus{}, [](auto&& e) { return e == state::hot;  });
+	for (auto&& ids : id_hits | view::filter([](auto&& v) { return v.size() > 1; })) {
+		for (auto id : ids) ids_intersected[id-1] = true;
+	}
+	std::cout << (1 + distance(begin(ids_intersected), 
+		            	       find_if(ids_intersected, [](auto&& p) { return p == false; })));
 }
