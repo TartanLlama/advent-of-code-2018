68325587a321156096b41b498f0e5f635cf5c73a
Simon Brand <tartanllama@gmail.com>
2018-12-03 16:03:15 +0000

    Improve day 2 part 2

--- advent_of_code_2018/2_inventory_management_system/part1.cpp
+++ advent_of_code_2018/2_inventory_management_system/part1.cpp
@@ -2,40 +2,27 @@
 #include <range/v3/istream_range.hpp>
 #include <range/v3/view/transform.hpp>
 #include <range/v3/numeric/accumulate.hpp>
+#include <range/v3/action/sort.hpp>
+#include <range/v3/algorithm/find_if.hpp>
+#include <range/v3/view/group_by.hpp>
 #include <iostream>
 #include <string>
-
-struct scan_result {
-    bool has_two = false;
-    bool has_three = false;
-};
-
-scan_result scan_box(std::string_view id) {
-    std::unordered_map<char, int> hits;
-    for (auto c : id) {
-        auto [_, inserted] = hits.emplace(c, 1);
-        if (!inserted) ++hits[c];
-    }
-    scan_result r;
-    for (auto [k,v] : hits) {
-        if (v == 2) r.has_two = true; 
-        else if (v == 3) r.has_three = true;
-    }
-    return r;
-}
 
 int main() {
     using namespace ranges;
     
-    struct count {
-        int n_twos = 0;
-        int n_threes = 0;
-    };
+	int pairs = 0;
+	int triples = 0;
 
-    auto accumulate_count = [](count c, scan_result s) { 
-                                return count{c.n_twos + s.has_two, c.n_threes + s.has_three}; 
-                            };
-    auto final_count = accumulate(istream_range<std::string>(std::cin) | view::transform(scan_box),
-                            count{}, accumulate_count);
-    std::cout << final_count.n_twos * final_count.n_threes << '\n';                    
+	for (auto&& s : istream_range<std::string>(std::cin)) {
+		auto groups = action::sort(s) | view::group_by(equal_to{});
+		auto has_group_of_size = [] (auto&& rng, std::size_t n) {
+			return find_if(rng, [n](auto&& g) { return distance(g) == n; }) != end(rng);
+		};
+		if (has_group_of_size(groups,2)) ++pairs;
+		if (has_group_of_size(groups,3)) ++triples;
+
+	}
+
+    std::cout << pairs * triples << '\n';                    
 }
